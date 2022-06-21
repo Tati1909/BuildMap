7a8dc26817cda9044ad720a963571f8f174c97bc
Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
2022-06-21 21:11:12 +0300

    1.6 Добавила маркеры на карту.  Сами места представляют собой список велосипедных магазинов в Сан-Франциско, Калифорния, США.

--- app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -2,11 +2,41 @@ package com.google.codelabs.buildyourfirstmap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.codelabs.buildyourfirstmap.place.Place
+import com.google.codelabs.buildyourfirstmap.place.PlacesReader
 
 class MainActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    /**
+     * Добавляет маркерные представления списка мест на предоставленном объекте GoogleMap.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+            )
+        }
     }
 }
--- app/src/main/java/com/google/codelabs/buildyourfirstmap/place/PlacesReader.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/place/PlacesReader.kt
@@ -1,17 +1,3 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.codelabs.buildyourfirstmap.place
 
 import android.content.Context
@@ -21,6 +7,10 @@ import com.google.gson.reflect.TypeToken
 import java.io.InputStream
 import java.io.InputStreamReader
 
+/**
+ *  Этот класс считывает список из 49 мест, которые хранятся в вызываемом файле places и возвращает их в виде файла List<Place>.
+ *  Сами места представляют собой список велосипедных магазинов в Сан-Франциско, Калифорния, США.
+ */
 class PlacesReader(private val context: Context) {
 
     private val gson = Gson()
