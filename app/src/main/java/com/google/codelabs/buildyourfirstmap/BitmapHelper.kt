6dcf7d0b777ff1f566acc9936a99351bcf0a1f91
Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
2022-06-22 20:36:35 +0300

    1.7 Настроила маркеры. Добавила адрес и рейтинг магазина

--- app/src/main/java/com/google/codelabs/buildyourfirstmap/BitmapHelper.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/BitmapHelper.kt
@@ -15,8 +15,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 object BitmapHelper {
     /**
-     * Demonstrates converting a [Drawable] to a [BitmapDescriptor], for use as a marker icon.
-     * Taken from ApiDemos on GitHub: https://github.com/googlemaps/android-samples/blob/master/ApiDemos/kotlin/app/src/main/java/com/example/kotlindemos/MarkerDemoActivity.kt
+    Этот метод принимает идентификатор ресурса, который можно нарисовать,
+    а также целое число цвета и создает его представление BitmapDescriptor .
      */
     fun vectorToBitmap(context: Context, @DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
         val vectorDrawable = ResourcesCompat.getDrawable(context.resources, id, null)
