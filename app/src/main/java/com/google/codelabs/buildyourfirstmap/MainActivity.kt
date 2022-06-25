60b0242b78c91ea3d9f77c2c866f2d79a6dd2612
Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
2022-06-25 11:21:59 +0300

    1.9 добавление библиотеки ktx, которая позволяет  использовать преимущества языковых функций Kotlin, таких как coroutines, свойства/функции extension и многое другое.

--- app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -2,26 +2,22 @@ package com.google.codelabs.buildyourfirstmap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.codelabs.buildyourfirstmap.place.Place
 import com.google.codelabs.buildyourfirstmap.place.PlaceRenderer
 import com.google.codelabs.buildyourfirstmap.place.PlacesReader
 import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.awaitMapLoad
 
 class MainActivity : AppCompatActivity() {
 
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
-    }
-
-    private val bicycleIcon: BitmapDescriptor by lazy {
-        val color = ContextCompat.getColor(this, R.color.colorPrimary)
-        BitmapHelper.vectorToBitmap(this, R.drawable.ic_directions_bike_black_24dp, color)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,17 +28,18 @@ class MainActivity : AppCompatActivity() {
             R.id.map_fragment
         ) as? SupportMapFragment
 
-        mapFragment?.getMapAsync { googleMap ->
-            addClusteredMarkers(googleMap)
+        lifecycleScope.launchWhenCreated {
+            // Get map
+            val googleMap = mapFragment?.awaitMap()
+            // Wait for map to finish loading
+            googleMap?.awaitMapLoad()
 
-            /**
-             * настройка вида камеры, чтобы он сразу инициализировался в Сан-Франциско при запуске приложения
-             */
-            googleMap.setOnMapLoadedCallback {
-                val bounds = LatLngBounds.builder()
-                places.forEach { bounds.include(it.latLng) }
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
-            }
+            // Ensure all places are visible in the map
+            val bounds = LatLngBounds.builder()
+            places.forEach { bounds.include(it.latLng) }
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+
+            googleMap?.let { addClusteredMarkers(it) }
         }
     }
 
